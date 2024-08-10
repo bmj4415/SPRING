71bf681a3d4a1ac59c8a865f1a488b59a72d9b58
bmj <bmj4415@gmail.com>
2024-08-11 01:30:25 +0900

    8/10 과제

---
+++ boot01/src/main/java/com/yedam/app/dept/service/Impl/DeptServiceImpl.java
@@ -0,0 +1,62 @@
+package com.yedam.app.dept.service.Impl;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.yedam.app.dept.mapper.DeptMapper;
+import com.yedam.app.dept.service.DeptService;
+import com.yedam.app.dept.service.DeptVO;
+
+@Service
+public class DeptServiceImpl implements DeptService{
+
+	private DeptMapper deptMapper;
+	
+	@Autowired
+	public DeptServiceImpl(DeptMapper deptMapper) {
+		this.deptMapper = deptMapper;
+	}
+
+	@Override
+	public List<DeptVO> deptList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DeptVO deptInfo(DeptVO deptVO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int deptInsert(DeptVO deptVO) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Map<String, Object> deptUpdate(DeptVO deptVO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<String, Object> deptDelete(int deptId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
